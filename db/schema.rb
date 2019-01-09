ee06875668184bb774bf4675bdc56e1b47c642ac
Mac Dowell Prosper <macdowell@noukod.com>
2019-01-09 19:16:34 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212205318) do
+ActiveRecord::Schema.define(version: 20190109191550) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "taxi_id"
   end
 
   create_table "rides", force: :cascade do |t|
